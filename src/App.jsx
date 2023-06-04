0c5128f6e4666a0bde3aa500350e0b10ec0435da
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-06-04 15:14:02 +0700

    bismillah, -initialcommit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Dashboard from './Dashboard';
+import Login from './Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
